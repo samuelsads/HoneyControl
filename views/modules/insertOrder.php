bce0594b601abffb5f30912ea0d9285e0e9a5026
samueldzib <samueldzibsads@gmail.com>
2017-04-25 20:52:53 -0500

    create insertOrders

---
+++ views/modules/insertOrder.php
@@ -0,0 +1,31 @@
+<?php 
+require_once "../../models/Crud.php";
+require_once "../../controllers/controllers.php";
+$responde  = new controllers();
+
+if($_SERVER['REQUEST_METHOD']=='POST') {
+	if(isset($_REQUEST['id']) && isset($_REQUEST['pass']) && isset($_REQUEST['quantity']) && isset($_REQUEST['discount'])  && isset($_REQUEST['total']) && isset($_REQUEST['pass'])){
+		$id =addslashes($_REQUEST['id']);
+		$contra = addslashes($_REQUEST['pass']);
+		if($contra == $responde ->getPass($id)){
+			$respuesta =$responde->getAllMyClient();
+				if($respuesta){
+					$respuesta = json_encode(array('respuesta'=>$respuesta,'success'=>true));
+					echo $respuesta;
+				}else{
+					echo 'error';
+				}
+		}else{
+			echo "incorrecto";
+		}
+		}else{
+			echo "error 1";
+		}
+	}else{
+		echo "error";
+	}
+	
+
+
+
+
